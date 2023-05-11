cd033faaffdeeb901978b7b5c59d0a43a36f45fb
Nathan Lefetey <lefetey.nathan@gmail.com>
2023-05-11 23:14:00 +0200

    :rocket: Deploy Shortcode

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# Retrieve Youtube Video Metadata
+This shortcode can help you getting metadata from one or few youtube videos
+
+# How to use it
+- Go on the [webpage](#)
+- Write the ids of video that you want to retrieve metadata
+- Then click on the button and accept downloads notification
+
+---
+***All the informations that you can retrieve with this code is viewable from youtube directly. It can just help you getting informations from few videos faster***
---
+++ script.js
@@ -0,0 +1,210 @@
+// const API_KEY = "AIzaSyAGIJpkJ2Q0OEgZcOIA-nDnDYsOqYNWygA";
+// const VIDEO_IDS_INPUT = document.getElementById("video-ids");
+// const START_SCRIPT = document.getElementById("start-script");
+
+// function fetchVideoData(VIDEO_ID) {
+//   const API_URL = `https://www.googleapis.com/youtube/v3/videos?id=${VIDEO_ID}&part=snippet,statistics,contentDetails&key=${API_KEY}`;
+
+//   return new Promise((resolve, reject) => {
+//     $.getJSON(API_URL, (data) => {
+//       if (data.items && data.items.length > 0) {
+//         resolve(data.items[0]);
+//       } else {
+//         reject("Video not found");
+//       }
+//     });
+//   });
+// }
+
+// function createXLSX(videoData) {
+//   const ws_name = "YouTube Video Data";
+//   const wb = XLSX.utils.book_new();
+
+//   const publishedAt = videoData.snippet.publishedAt;
+//   const dateObj = new Date(publishedAt);
+//   const formattedDate = dateObj.toLocaleDateString("fr-FR", {
+//     day: "2-digit",
+//     month: "2-digit",
+//     year: "numeric"
+//   });
+//   const formattedTime = dateObj.toLocaleTimeString("fr-FR", {
+//     hour: "2-digit",
+//     minute: "2-digit",
+//     second: "2-digit"
+//   });
+
+//   const duration = videoData.contentDetails.duration;
+//   const match = duration.match(/PT(\d+H)?(\d+M)?(\d+S)?/);
+//   let hours = 0;
+//   let minutes = 0;
+//   let seconds = 0;
+  
+//   if (match) {
+//     if (match[1]) {
+//       hours = parseInt(match[1].slice(0, -1));
+//     }
+//     if (match[2]) {
+//       minutes = parseInt(match[2].slice(0, -1));
+//     }
+//     if (match[3]) {
+//       seconds = parseInt(match[3].slice(0, -1));
+//     }
+//   }
+  
+//   const formattedDuration = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+  
+
+//   const ws_data = [
+//     [
+//       "Video ID",
+//       videoData.id,
+//     ],
+//     [
+//       "Title",
+//       videoData.snippet.title,
+//     ],
+//     [
+//       "Description",
+//       videoData.snippet.description,
+//     ],
+//     [
+//       "Channel ID",
+//       videoData.snippet.channelId,
+//     ],
+//     [
+//       "Channel Title",
+//       videoData.snippet.channelTitle,
+//     ],
+//     [
+//       "Published At",
+//       // date + " " + time,
+//       formattedDate + " " + formattedTime
+//     ],
+//     [
+//       "Duration",
+//       formattedDuration
+//     ],
+//     [
+//       "View Count",
+//       videoData.statistics.viewCount,
+//     ],
+//   ];
+
+//   const ws = XLSX.utils.aoa_to_sheet(ws_data);
+//   XLSX.utils.book_append_sheet(wb, ws, ws_name);
+//   XLSX.writeFile(wb, `youtube-video-data.xlsx`);
+// }
+
+// START_SCRIPT.addEventListener("click", () => {
+//   const videoIds = VIDEO_IDS_INPUT.value.split(' ');
+//   console.log(videoIds);
+//   videoIds.forEach((videoId) => {
+//     fetchVideoData(videoId)
+//       .then((videoData) => {
+//         createXLSX(videoData);
+//       })
+//       .catch((error) => {
+//         console.error(error);
+//       });
+//   });
+// });
+
+
+// 1UaOnPvwrd0
+// WyWJ77J_epE
+// _i2obqmj6vY
+
+
+const API_KEY = "AIzaSyAGIJpkJ2Q0OEgZcOIA-nDnDYsOqYNWygA";
+const VIDEO_IDS_INPUT = document.getElementById("video-ids");
+const START_SCRIPT = document.getElementById("start-script");
+
+function fetchVideoData(VIDEO_ID) {
+  const API_URL = `https://www.googleapis.com/youtube/v3/videos?id=${VIDEO_ID}&part=snippet,statistics,contentDetails&key=${API_KEY}`;
+
+  return new Promise((resolve, reject) => {
+    $.getJSON(API_URL, (data) => {
+      if (data.items && data.items.length > 0) {
+        resolve(data.items[0]);
+      } else {
+        reject("Video not found");
+      }
+    });
+  });
+}
+
+function createXLSX(videoDataList) {
+  const ws_name = "YouTube Video Data";
+  const wb = XLSX.utils.book_new();
+  let ws_data = [];
+
+  videoDataList.forEach((videoData, index) => {
+    const publishedAt = videoData.snippet.publishedAt;
+    const dateObj = new Date(publishedAt);
+    const formattedDate = dateObj.toLocaleDateString("fr-FR", {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric"
+    });
+    const formattedTime = dateObj.toLocaleTimeString("fr-FR", {
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit"
+    });
+  
+    const duration = videoData.contentDetails.duration;
+    const match = duration.match(/PT(\d+H)?(\d+M)?(\d+S)?/);
+    let hours = 0;
+    let minutes = 0;
+    let seconds = 0;
+    
+    if (match) {
+      if (match[1]) {
+        hours = parseInt(match[1].slice(0, -1));
+      }
+      if (match[2]) {
+        minutes = parseInt(match[2].slice(0, -1));
+      }
+      if (match[3]) {
+        seconds = parseInt(match[3].slice(0, -1));
+      }
+    }
+    
+    const formattedDuration = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+
+    const data = [
+      ["Video ID", videoData.id],
+      ["Title", videoData.snippet.title],
+      ["Description", videoData.snippet.description],
+      ["Channel ID", videoData.snippet.channelId],
+      ["Channel Title", videoData.snippet.channelTitle],
+      ["Published At", formattedDate + " " + formattedTime],
+      ["Duration", formattedDuration],
+      ["View Count", videoData.statistics.viewCount],
+    ];
+
+    ws_data.push(...data);
+
+    // Ajouter une ligne vide entre chaque vidéo, sauf pour la dernière
+    if (index < videoDataList.length - 1) {
+      ws_data.push([]);
+    }
+  });
+
+  const ws = XLSX.utils.aoa_to_sheet(ws_data);
+  XLSX.utils.book_append_sheet(wb, ws, ws_name);
+  XLSX.writeFile(wb, `youtube-videos-data.xlsx`);
+}
+
+START_SCRIPT.addEventListener("click", () => {
+  const videoIds = VIDEO_IDS_INPUT.value.split(" ");
+  const videoDataPromises = videoIds.map((videoId) => fetchVideoData(videoId));
+
+  Promise.all(videoDataPromises)
+    .then((videoDataList) => {
+      createXLSX(videoDataList);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
